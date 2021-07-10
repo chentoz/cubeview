3d7087ccd35c54351a74ef4fd0b98f044fd30e78
chentoz <492309597@qq.com>
2021-07-10 16:41:46 +0800

    mousemove

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
+//import Hello from './Hello';
+import App from './App'
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'))
